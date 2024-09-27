28ccb722a00d5632cc9ae93689213d87c514b216
Jonathan Andres Garcia Rodriguez <andres.estudio.123@gamil.com>
2024-09-26 21:56:01 -0500

    feat(front):New Design

--- Actividad3/index.php
+++ Actividad3/index.php
@@ -9,22 +9,39 @@
     
   </head>
   <body>
-    <div class="tittle">
-        <h1 class="text-center">Hello, world!</h1>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">USABILIDAD WEB</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="form.php">Formulario</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Actividad 1</a></li>
+              <li><a class="dropdown-item" href="#">Actividad 2</a></li>
+              <li><a class="dropdown-item" href="#">Actividad 3</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          
+        </ul>
+       
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <div class="container text-center">
-        <div class="row align-items-start">
-            <div class="col">
-            One of three columns
-            </div>
-            <div class="col">
-            One of three columns
-            </div>
-            <div class="col">
-            One of three columns
-        </div>
-  </div>
-</div>
+  </nav>
   </body>
 </html>
