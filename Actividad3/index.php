7be10154890e9737666db11e517bdb77c60dc52b
Jonathan Andres Garcia Rodriguez <andres.estudio.123@gamil.com>
2024-09-27 22:12:30 -0500

    feat(front):Lo que quiera

--- Actividad3/index.php
+++ Actividad3/index.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="#">
     
   </head>
   <body>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
