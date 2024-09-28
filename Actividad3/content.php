c1a064e4cfb103117dad347810281a7df605a661
Yenner Alayon <yenneralayon@gmail.com>
2024-09-27 21:25:59 -0500

    feat(front): Add content and new structure

---
+++ Actividad3/content.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <main>
+        <h1 class="text-center">Teoria Usabilidad y Desarrollo de Aplicaciones Web</h1><br>
+        <div>
+            <h2>¿Qué es la usabilidad Web?</h2>
+            <p>
+                La usabilidad web se refiere a la facilidad de uso que tiene una página o aplicación web. De esta forma los internautas interactúan con el sitio y consiguen sus objetivos de forma sencilla, intuitiva, agradable y segura.
+            </p>
+        </div>
+
+        <div>
+            <h2>¿Por qué es importante la usabilidad Web?</h2>
+            <p>
+                Cuando una persona accede una pagina y esta no es fácil de usar,  es abandonada.
+                Si su sitio web no facilita la navegación, dificulta alcanzar los objetivos, carece de una estructura que permita escanear rápidamente la información o carga de manera lenta, es muy probable que los usuarios abandonen la página antes de terminar de leer el primer párrafo.
+            </p>
+        </div>
+
+        <div>
+            <h2>Principios de la Usabilidad Web</h2>
+            <p>
+                Los 10 principios de la usabilidad web por Jacob Nielsen:
+                <ul>
+                    <li>Visibilidad del estado del sistema</li>
+                    <li>Relación entre el sistema y el mundo real</li>
+                    <li>Control y libertad del usuario</li>
+                    <li>Consistencia y estándares</li>
+                    <li>Prevención de errores</li>
+                    <li>Reconocer antes que recordar</li>
+                    <li>Flexibilidad y eficiencia de uso</li>
+                    <li>Diseño estético y minimalista</li>
+                    <li>Ayudar a los usuarios a reconocer, diagnosticar y corregir los errores</li>
+                    <li>Ayuda y documentación</li>
+                </ul>
+            </p>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
