aed82e5de31af6e19b6f7aed47bdad62bfd0cc37
Jonathan Andres Garcia Rodriguez <andres.estudio.123@gamil.com>
2024-10-14 20:01:18 -0500

    feat (font): Add Favicon

--- Actividad3/index.php
+++ Actividad3/index.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="start/png" href="/public/start.png" />
+    <link rel="icon" type="start/png" href="public/icon.png" />
     <title>Usabilidad Web</title>
     <?php
         include 'src/layouts/header.php';  // Incluye el encabezado
