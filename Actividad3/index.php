4f2baa5b5438618fc897aa2af1f09d1f9027ced7
Yenner Alayon <yenneralayon@gmail.com>
2024-10-16 20:11:27 -0500

    feat(front): fix styles

--- Actividad3/index.php
+++ Actividad3/index.php
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="start/png" href="public/icon.png" />
-    <title>Usabilidad Web</title>
+    <title>Sense Digital</title>
     <?php
         include 'src/layouts/header.php';  // Incluye el encabezado
     ?>
