5a8c9b7263ea8582119756b07b654e651a89787a
Yenner Alayon <yenneralayon@gmail.com>
2025-03-29 23:52:53 -0500

    feat(front and back): Add crud finalization

--- CrudPHP/index.php
+++ CrudPHP/index.php
@@ -1,5 +1,5 @@
 <?php
-include('config/connection.php');
+include('connection.php');
 $con = connection();
 $sql = "SELECT * from users";
 $query = mysqli_query($con,$sql);
@@ -13,26 +13,27 @@ if (!$query) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Waplicaciones</title>
 </head>
 <body>
-     <main>
+     <main class="users-form">
         <h1>Registro de Usuarios</h1>
-            <form action="views/insert_user.php" method="POST"> 
+            <form action="insert_user.php" method="POST"> 
                 <label for="name">Nombre</label>
-                <input type="text" placeholder="Yenner" id="name">
+                <input type="text" placeholder="Yenner" id="name" name="name">
                 <label for="lastname">Apellido</label>
-                <input type="text" placeholder="Alayon" id="lastname">
+                <input type="text" placeholder="Alayon" id="lastname" name="lastname">
                 <label for="username">Username</label>
-                <input type="text" placeholder="yalayon" id="username">
+                <input type="text" placeholder="yalayon" id="username" name="username">
                 <label for="password">Password</label>
-                <input type="text" placeholder="12345" >
+                <input type="text" placeholder="12345" name="password">
                 <label for="email">Email</label>
-                <input type="text" placeholder="yenneralayon@gmail.com">
+                <input type="text" placeholder="yenneralayon@gmail.com" name="email">
                 <input type="submit" value="Agregar Usuario">
             </form>
      </main>
-     <section>
+     <section class="users-table">
         <h2>Usuarios Registrados</h2>
             <table>
                 <thead>
@@ -56,8 +57,8 @@ if (!$query) {
                                 <td><?= $row['username'] ?></td>
                                 <td><?= $row['password'] ?></td>
                                 <td><?= $row['email'] ?></td>
-                                <td><a href="">Editar</a></td>
-                                <td><a href="">Eliminar</a></td>
+                                <td><a href="update.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></td>
+                                <td><a href="delete_user.php?id=<?= $row['id'] ?>" class="users-table--delete">Eliminar</a></td>
                             </tr>
                     <?php endwhile; ?>
                 </tbody>
